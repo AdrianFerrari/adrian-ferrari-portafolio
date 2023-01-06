fcc0f2e09af8eae10426428afd9c5ccffd659934
Adrian Ferrari <sdadrian@gmail.com>
2023-01-06 01:20:19 -0300

    .

--- app/proyectos/page.jsx
+++ app/proyectos/page.jsx
@@ -1,15 +1,38 @@
+"use client";
 import Image from "next/image"
 import style from "./proyectos.module.css"
-import pastry from "../../public/screenshots/pastry_shop.jpeg"
-import chat_app from "../../public/screenshots/chat_app_side.jpeg"
+import useSWR from "swr"
+import ProjectModalCard from "../components/ProjectModalCard";
+
+//imagenes
+import pastry_shop_home from "../../public/screenshots/pastry_shop_home.jpeg"
+import chat_app from "../../public/screenshots/chat_app.jpeg"
 import quiz_app from "../../public/screenshots/quiz_app.jpeg"
 import tenzies from "../../public/screenshots/tenzies.jpeg"
 import pics_shop from "../../public/screenshots/pics_shop.jpeg"
-import supplies_crud from "../../public/screenshots/supplies_crud_side.jpeg"
+import supplies_crud from "../../public/screenshots/supplies_crud.jpeg"
 import profile_card from "../../public/screenshots/profile_card.jpeg"
 import color_picker from "../../public/screenshots/color_picker.jpeg"
 
+async function getData(...args) {
+    const res = await fetch(...args)
+    if(!res.ok) throw new Error('Failed to fetch data');
+    return res.json()
+}
+
 export default function Proyectos() {
+    const { data, error, isLoading } = useSWR('/api/projectosdata', getData);
+    if (error) return <div>Failed to load</div>;
+    if (isLoading) return <div>Loading...</div>
+
+    const proyectsData = JSON.parse(data)
+    const dataHTML = proyectsData.map((item, i) => {
+        return(
+            <ProjectModalCard key={i} {...item}/>
+        )
+    })
+    
+
     return(
         <div className={style.proyectos}>
             <h1>PROYECTOS</h1>
@@ -17,7 +40,7 @@ export default function Proyectos() {
             <div className={style.proyectos_grid}>
                 <Image 
                     className={[style.images, style.pastry].join(" ")}
-                    src={pastry}
+                    src={pastry_shop_home}
                     alt="pastry shop"
                     placeholder="blur"
                 />
@@ -34,7 +57,7 @@ export default function Proyectos() {
                     placeholder="blur"
                 />
                 <Image 
-                    className={[style.images, style.picture].join(" ")}
+                    className={[style.images, style.pics].join(" ")}
                     src={pics_shop}
                     alt="pictures shop"
                     placeholder="blur"
