2f59fb6d59062cc5f4ecf4b61027140c0dc0ae8f
Adrian Ferrari <sdadrian@gmail.com>
2023-01-27 16:37:48 -0300

    move window listener inside an useEffect

--- app/(rutas)/template.jsx
+++ app/(rutas)/template.jsx
@@ -1,11 +1,12 @@
 "use client"
 import { motion } from "framer-motion";
 import { usePathname } from 'next/navigation';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 let lastIndex = 0
 
 export default function Template({ children }) {
+    const [windowWidth, setWindowWidth] = useState(null)
     const paginas = ["/home", "/about", "/competencias", "/proyectos", "/contacto"]
     const pathname = usePathname()
     const currentIndex = paginas.indexOf(pathname)
@@ -13,6 +14,18 @@ export default function Template({ children }) {
     useEffect(() => {
         lastIndex = paginas.indexOf(pathname)
     }, [pathname])
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+      
+        window.addEventListener('resize', handleWindowResize);
+      
+        return () => {
+        window.removeEventListener('resize', handleWindowResize);
+        };
+    })
 
     const deArriba = [{ y: 300, opacity: 0}, { y: 0, opacity: 1}]
     const deAbajo = [{ y: -300, opacity: 0}, { y: 0, opacity: 1}]
@@ -24,8 +37,8 @@ export default function Template({ children }) {
         <motion.div
             key={pathname}
             className="pages_motion" 
-            initial={window.innerWidth < 660 ? (currentIndex > lastIndex ? deDerecha[0] : deIzquierda[0]) : currentIndex > lastIndex ? deArriba[0] : deAbajo[0]}
-            animate={window.innerWidth < 660 ? (currentIndex > lastIndex ? deDerecha[1] : deIzquierda[1]) : currentIndex > lastIndex ? deArriba[1] : deAbajo[1]}
+            initial={windowWidth < 660 ? (currentIndex > lastIndex ? deDerecha[0] : deIzquierda[0]) : currentIndex > lastIndex ? deArriba[0] : deAbajo[0]}
+            animate={windowWidth < 660 ? (currentIndex > lastIndex ? deDerecha[1] : deIzquierda[1]) : currentIndex > lastIndex ? deArriba[1] : deAbajo[1]}
             exit={{ x: 300, opacity: 0 }}
             layout
         >
