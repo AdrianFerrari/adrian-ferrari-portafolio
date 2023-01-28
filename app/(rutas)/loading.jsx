9ee6804f4dadee448f7083184c5534f31768926d
Adrian Ferrari <sdadrian@gmail.com>
2023-01-28 15:43:46 -0300

    .

--- app/(rutas)/loading.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./loading.module.css"
-
-export default function Loading() {
-    return (
-        <div className={style.loading_container}>
-            <div class={style.lds_ring}><div></div><div></div><div></div><div></div></div>
-        </div>
-    )
-}
