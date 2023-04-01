412d3b0280948be0acb0d190d302c0039785b9cb
Adrian Ferrari <sdadrian@gmail.com>
2023-04-01 11:37:33 -0300

    syntax errors, removed 'he' package, minor fixes

--- app/(rutas)/components/ProjectCard.jsx
+++ app/(rutas)/components/ProjectCard.jsx
@@ -2,59 +2,47 @@
 import Image from "next/image";
 import Link from "next/link";
 import style from "./styles/projectcard.module.css";
-import he from "he";
 import GlobalSVG from "public/icons/global.svg";
 import GitHubSVG from "public/icons/github.svg";
 
+export default function ProjectCard({ title, id, description, tags, screenshots, links }) {
+  return (
+    <div className={style.project_card + " " + style[id]}>
+      <Image
+        className={style.image}
+        src={require(`public/screenshots/${screenshots[0]}.png`)}
+        alt={`${id} app`}
+        placeholder="blur"
+      />
 
-export default function ProjectCard({
-    title,
-    id,
-    description,
-    tags,
-    screenshots,
-    links,
-}) {
-    
-    return (
-        <div className={style.project_card + " " + style[id]}>
-            <Image
-                className={style.image}
-                src={require(`public/screenshots/${screenshots[0]}.png`)}
-                alt={`${id} app`}
-                placeholder="blur"
-            />
+      <div className={style.titleholder}>
+        <h3 className={style.title}>{title}</h3>
+      </div>
 
-            <div className={style.titleholder}>
-                <h3 className={style.title}>{title}</h3>
-            </div>
-
-            <div className={style.info}>
-                        <p>{he.decode(description)}</p>
-                        <div className={style.info_tags}>{tags.map(tag => {
-                            return (
-                                <div className={style.info_tag} key={tag}>{tag}</div>
-                            )
-                            })}
-                        </div>
-            </div>
-
-            <div className={style.buttons}>
-                {links.map((item) => {
-                    return (
-                        <Link
-                            target="_blank"
-                            href={item.link}
-                            key={item.name}
-                            className={style[item.name.toLowerCase()]}
-                        >
-                            {item.name === "Github" && <GitHubSVG width={24} height={24}/>}
-                            {item.name === "Website" && <GlobalSVG width={24} height={24}/>}
-                            {item.name}
-                        </Link>
-                    );
-                })}
-            </div>
+      <div className={style.info}>
+        <p>{description}</p>
+        <div className={style.info_tags}>
+          {tags.map((tag) => {
+            return (
+              <div className={style.info_tag} key={tag}>
+                {tag}
+              </div>
+            );
+          })}
         </div>
-    );
+      </div>
+
+      <div className={style.buttons}>
+        {links.map((item) => {
+          return (
+            <Link target="_blank" href={item.link} key={item.name} className={style[item.name.toLowerCase()]}>
+              {item.name === "Github" && <GitHubSVG width={24} height={24} />}
+              {item.name === "Website" && <GlobalSVG width={24} height={24} />}
+              {item.name}
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- app/(rutas)/proyectos/page.jsx
+++ app/(rutas)/proyectos/page.jsx
@@ -1,37 +1,40 @@
 "use client";
-import style from "./proyectos.module.css"
-import useSWR from "swr"
+import style from "./proyectos.module.css";
+import useSWR from "swr";
 import ProjectCard from "../components/ProjectCard";
-import PageHead from "../components/PageHead"
+import PageHead from "../components/PageHead";
 
 async function getData(...args) {
-    const res = await fetch(...args)
-    if(!res.ok) throw new Error('Failed to fetch data');
-    return res.json()
+  const res = await fetch(...args);
+  if (!res.ok) throw new Error("Failed to fetch data");
+  return res.json();
 }
 
 export default function Proyectos() {
-    const { data, error, isLoading } = useSWR('/api/projectosdata', getData);
-    if (error) return <div>Failed to load</div>;
-    if (isLoading) return <div className="loading-data-screen"><span>Loading...</span></div>
+  const { data, error, isLoading } = useSWR("/api/projectosdata", getData);
+  if (error) return <div>Failed to load</div>;
+  if (isLoading)
+    return (
+      <div className="loading-data-screen">
+        <span>Loading...</span>
+      </div>
+    );
 
-    const proyectsData = JSON.parse(data)
-    const dataHTML = proyectsData.map((item, i) => {
-        return(
-            <div key={i} className={style.card}>
-                <ProjectCard {...item}/>
-            </div>
-        )
-    })
-    
-    return(
-        <main className="page_container">
-            <PageHead nombre="Proyectos"/>
-            <div className={style.proyectos_grid}>
-                {
-                    dataHTML
-                }
-            </div>
-        </main>
-    )
+  const proyectsData = JSON.parse(data);
+  console.log("data", data);
+  console.log("parsedData", proyectsData);
+  const dataHTML = proyectsData.map((item, i) => {
+    return (
+      <div key={i} className={style.card}>
+        <ProjectCard {...item} />
+      </div>
+    );
+  });
+
+  return (
+    <main className="page_container">
+      <PageHead nombre="Proyectos" />
+      <div className={style.proyectos_grid}>{dataHTML}</div>
+    </main>
+  );
 }
